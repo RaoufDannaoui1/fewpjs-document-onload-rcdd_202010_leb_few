cd97817038e8d898c140ce72a06be33c200bcc2a
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2020-11-19 20:54:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", call()
+ {
+  console.log("The DOM has loaded");
+});
+ 
+function call() {
+  document.querySelector("text").textContent="This is really cool!";
+}
