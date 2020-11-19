b6ea902e74b98a9951a31e2216245927b2863772
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2020-11-19 20:54:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", call()
  {
   console.log("The DOM has loaded");
 });
- 
+
 function call() {
   document.querySelector("text").textContent="This is really cool!";
 }
